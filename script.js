308e8e6204437a4c5d7e8bf7f2df67937929d0c9
cdixon-lab <cdixon@my.campus.edu>
2026-09-14 22:28:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,17 @@ const housingToggle = document.querySelector("#housing-toggle");
 const housingInfo = document.querySelector("#housing-info");
 
 housingToggle.addEventListener("click", function () {
-if (housingDetails.style.display =="none") {
-    housingDetails.style.display = "block";
-    housingToggle.textContent = "Hide Housing Details";
-} else {
-         housingDetails.style.display = "none";
-         housingToggle.textContent = "Show Housing Details";
-}
+const isHidden = housingInfo.hidden;
+
+    housingInfo.hidden = !isHidden;
+    housingToggle.setAttribute("aria-expanded" , String(isHidden));
+
+    if (isHidden) {
+             housingToggle.textContent = "Hide Housing Details";
+    } else {
+        housingToggle.textContent = "Show Housing Details";
+    }
 });
-        const isHidden = housingInfo.hidden;
-
-        housingInfo.hidden = !isHidden;
-        housingToggle.setAttribute("aria-expanded", String(isHidden));
-
-        if (isHidden) {
-            housingToggle.textContent = "Hide Housing Details";
-        } else {
-            housingToggle.textContent = "Show Housing Details";
-        }
-    });
-}
 
 const contactForm = document.querySelector("#contact-form");
 
